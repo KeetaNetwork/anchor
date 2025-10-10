0aa836f4b083344c289bfe68d8ae40a78541fc1e
Tanveer Wahid <twahid@keeta.com>
2025-10-10 13:55:14 -0700

    Undo unnecessary eslint config changes.

--- .eslint.config.mjs
+++ .eslint.config.mjs
@@ -1,19 +1,5 @@
 import keetanetworkConfig from '@keetanetwork/eslint-config-typescript';
-import tseslint from '@typescript-eslint/eslint-plugin';
 
 export default [
-	...keetanetworkConfig,
-	{
-		files: ['**/*.ts', '**/*.tsx'],
-		languageOptions: {
-			parser: (await import('@typescript-eslint/parser')).default,
-			parserOptions: {
-				ecmaVersion: 'latest',
-				sourceType: 'module'
-			}
-		},
-		plugins: {
-			'@typescript-eslint': tseslint
-		}
-	}
+	...keetanetworkConfig
 ];
