e5462e7a0f9d036c3af31afd9ddd2b14ebd25c0c
Roy Keene <rkeene@keeta.com>
2025-10-21 15:12:52 -0500

    Fix linting issues

--- src/lib/log/index.ts
+++ src/lib/log/index.ts
@@ -2,8 +2,8 @@ import { lib } from '@keetanetwork/keetanet-client';
 export type * from '@keetanetwork/keetanet-client/lib/log/index.js';
 
 const Log: typeof lib.Log = lib.Log;
-const LegacyLog: typeof Log.Legacy = Log.Legacy;
-const NullLog: typeof Log.Null = Log.Null;
+const LegacyLog: typeof Log.Legacy = Log.Legacy.bind(Log);
+const NullLog: typeof Log.Null = Log.Null.bind(Log);
 
 export {
 	Log,
