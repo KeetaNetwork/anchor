90b382fcf0d622ae140e46224b571c5ff504639d
Roy Keene <rkeene@keeta.com>
2025-10-27 18:49:19 -0500

    Upgrade to latest KeetaNet (v0.14.11)

--- src/lib/utils/crypto.ts
+++ src/lib/utils/crypto.ts
@@ -1,14 +1,5 @@
 import { lib as KeetaNetLib } from '@keetanetwork/keetanet-client';
 
-// XXX:TODO: Eliminate when we get native support in KeetaNetLib
-import * as nodeCrypto from 'node:crypto';
+const crypto: typeof KeetaNetLib.Utils.Helper.crypto = KeetaNetLib.Utils.Helper.crypto;
 
-const crypto: typeof KeetaNetLib.Utils.Helper.crypto & {
-	createHash: typeof nodeCrypto.createHash;
-	createHmac: typeof nodeCrypto.createHmac;
-} = {
-	...KeetaNetLib.Utils.Helper.crypto,
-	createHash: nodeCrypto.createHash.bind(nodeCrypto),
-	createHmac: nodeCrypto.createHmac.bind(nodeCrypto)
-};
 export default crypto;
