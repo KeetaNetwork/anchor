aa0f8b45e805224521e320ff4a76a34db1fc87ca
Roy Keene <rkeene@keeta.com>
2025-10-30 17:03:54 -0500

    Improved DeepPartial

--- src/lib/utils/types.ts
+++ src/lib/utils/types.ts
@@ -1,3 +1,6 @@
-export type DeepPartial<T> = T extends object ? {
-	[P in keyof T]?: DeepPartial<T[P]>;
-} : T;
+export type DeepPartial<T> =
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	T extends (...args: any[]) => any ? T :
+		T extends (infer U)[] ? DeepPartial<U>[] :
+			T extends object ? { [P in keyof T]?: DeepPartial<T[P]> } :
+				T;
