017cadb6f039fe82a12237d0c945249e7d2591cc
Roy Keene <rkeene@keeta.com>
2025-10-30 23:40:55 -0500

    Start of work

--- src/lib/error.test.ts
+++ src/lib/error.test.ts
@@ -1,14 +1,23 @@
 import { test, expect } from 'vitest';
-import { KeetaAnchorUserError } from './error.js';
+import { KeetaAnchorUserError, KeetaAnchorError } from './error.js';
 
 test('Basic Error Test', async function() {
+	const keetaAnchorError = new KeetaAnchorError('test error');
 	const keetaAnchorUserError = new KeetaAnchorUserError('test error');
 
 	expect(KeetaAnchorUserError.isInstance(keetaAnchorUserError)).toBe(true);
+	expect(KeetaAnchorError.isInstance(keetaAnchorUserError)).toBe(true);
+	expect(KeetaAnchorError.isInstance(keetaAnchorError)).toBe(true);
+	expect(KeetaAnchorUserError.isInstance(keetaAnchorError)).toBe(false);
 
 	expect(keetaAnchorUserError.message).toBe('test error');
+	expect(keetaAnchorUserError.name).toBe('KeetaAnchorUserError');
+	expect(keetaAnchorError.message).toBe('test error');
+	expect(keetaAnchorError.name).toBe('KeetaAnchorError');
 
 	expect(keetaAnchorUserError.asErrorResponse('text/plain')).toEqual({ error: 'test error', statusCode: 400, contentType: 'text/plain' });
+	expect(keetaAnchorError.asErrorResponse('text/plain')).toEqual({ error: 'Internal error', statusCode: 400, contentType: 'text/plain' });
 
-	expect(keetaAnchorUserError.asErrorResponse('application/json')).toEqual({ error: JSON.stringify({ ok: false, error: 'test error' }), statusCode: 400, contentType: 'application/json' });
+	expect(keetaAnchorUserError.asErrorResponse('application/json')).toEqual({ error: JSON.stringify({ ok: false, retryable: false, error: 'test error' }), statusCode: 400, contentType: 'application/json' });
+	expect(keetaAnchorError.asErrorResponse('application/json')).toEqual({ error: JSON.stringify({ ok: false, retryable: false, error: 'Internal error' }), statusCode: 400, contentType: 'application/json' });
 });
