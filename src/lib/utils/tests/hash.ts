4847ff44cc343830b82210679c9df820c7f707fd
Roy Keene <rkeene@keeta.com>
2025-12-18 03:10:32 -0600

    Ensure that hash length is a positive value

--- src/lib/utils/tests/hash.ts
+++ src/lib/utils/tests/hash.ts
@@ -1,6 +1,10 @@
 import * as crypto from 'crypto';
 
 export function hash(input: string, length: number = Infinity): string {
+	if (length <= 0) {
+		throw(new Error('Length must be a positive integer'));
+	}
+
 	const hasher = crypto.createHash('sha256').update(input);
 
 	return(hasher.digest('hex').slice(0, length));
