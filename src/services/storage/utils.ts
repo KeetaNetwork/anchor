1ed915d0e15fe6fd2b3655cf24eafc30f70fa2fa
Tanveer Wahid <twahid@keeta.com>
2026-02-19 12:10:55 -0800

    Fix lint.

--- src/services/storage/utils.ts
+++ src/services/storage/utils.ts
@@ -5,8 +5,9 @@ const VALID_VISIBILITIES = new Set<string>(['public', 'private']);
 
 export function assertVisibility(value: unknown): StorageObjectVisibility {
 	if (typeof value !== 'string' || !VALID_VISIBILITIES.has(value)) {
-		throw new Errors.InvalidMetadata(`visibility must be 'public' or 'private'`);
+		throw(new Errors.InvalidMetadata(`visibility must be 'public' or 'private'`));
 	}
 
+	// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
 	return(value as StorageObjectVisibility);
 }
