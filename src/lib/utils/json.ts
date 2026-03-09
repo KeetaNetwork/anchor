b2ab0224547c9635c389ab30f9b6f25641b9903c
Ryan Staples <rstaples@keeta.com>
2026-03-09 12:13:43 -0400

    export toJSONSerializable and debugPrintableObject from Node

--- src/lib/utils/json.ts
+++ src/lib/utils/json.ts
@@ -1,4 +1,6 @@
-import { type JSONSerializable, type ToJSONSerializable, toJSONSerializable } from '@keetanetwork/keetanet-client/lib/utils/conversion.js';
+import { type JSONSerializable, type ToJSONSerializable, toJSONSerializable } from '@keetanetwork/keetanet-node/dist/lib/utils/conversion.js';
+import { debugPrintableObject } from '@keetanetwork/keetanet-node/dist/lib/utils/helper.js';
+
 export type { JSONSerializable, ToJSONSerializable };
 
-export { toJSONSerializable };
+export { toJSONSerializable, debugPrintableObject };
