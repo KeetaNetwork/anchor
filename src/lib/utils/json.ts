fa8ee2a2bfeba8e482c8d89868beb106022dab04
Roy Keene <rkeene@keeta.com>
2026-03-17 12:08:36 -0500

    Fix toJSONSerializable and KeetaNet import

--- src/lib/utils/json.ts
+++ src/lib/utils/json.ts
@@ -1,8 +1,8 @@
-import { type JSONSerializable, type ToJSONSerializable, toJSONSerializable } from '@keetanetwork/keetanet-client/lib/utils/conversion.js';
-import { KeetaNet } from '../../client/index.js';
+import type { JSONSerializable, ToJSONSerializable } from '@keetanetwork/keetanet-client/lib/utils/conversion.js';
+import * as KeetaNet from '@keetanetwork/keetanet-client';
 
 const debugPrintableObject: (input: any) => JSONSerializable = KeetaNet.lib.Utils.Helper.debugPrintableObject;
+const toJSONSerializable: typeof KeetaNet.lib.Utils.Conversion.toJSONSerializable = KeetaNet.lib.Utils.Conversion.toJSONSerializable;
 
 export type { JSONSerializable, ToJSONSerializable };
-
 export { toJSONSerializable, debugPrintableObject };
