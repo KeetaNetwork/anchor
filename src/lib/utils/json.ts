192904d3bd72120a7f9fcb870a34692925622a82
Roy Keene <rkeene@keeta.com>
2026-03-17 12:11:10 -0500

    Get rid of (hide) "any"

--- src/lib/utils/json.ts
+++ src/lib/utils/json.ts
@@ -1,7 +1,7 @@
 import type { JSONSerializable, ToJSONSerializable } from '@keetanetwork/keetanet-client/lib/utils/conversion.js';
 import * as KeetaNet from '@keetanetwork/keetanet-client';
 
-const debugPrintableObject: (input: any) => JSONSerializable = KeetaNet.lib.Utils.Helper.debugPrintableObject;
+const debugPrintableObject: typeof KeetaNet.lib.Utils.Helper.debugPrintableObject = KeetaNet.lib.Utils.Helper.debugPrintableObject;
 const toJSONSerializable: typeof KeetaNet.lib.Utils.Conversion.toJSONSerializable = KeetaNet.lib.Utils.Conversion.toJSONSerializable;
 
 export type { JSONSerializable, ToJSONSerializable };
