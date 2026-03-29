aa317c92ecc5d93f0247a1768978235512943465
ezra ripps <eripps@keeta.com>
2026-03-29 19:03:16 -0400

    linting

--- src/services/asset-movement/lib/data/addresses/bank-account/fps.ts
+++ src/services/asset-movement/lib/data/addresses/bank-account/fps.ts
@@ -2,36 +2,36 @@ import { sharedSchemaReferences, type AccountAddressSchema } from "../../types.j
 
 // UK Faster payments schema
 const fpsSchema: AccountAddressSchema = {
-    type: 'bank-account',
+	type: 'bank-account',
 
-    includeFields: {
-        accountOwner: true,
-        accountNumberEnding: true
-    },
+	includeFields: {
+		accountOwner: true,
+		accountNumberEnding: true
+	},
 
-    additionalProperties: {
-        resolved: {
-            type: "object",
-            properties: {
-                accountAddress: sharedSchemaReferences.PhysicalAddress,
-                accountNumber: { type: "string", pattern: "^[a-zA-Z0-9]{2,80}$" },
-                sortCode: { type: "string" }
-            },
-            required: []
-        },
-        obfuscated: {
-            type: "object",
-            properties: {
-                sortCode: { type: "string" },
-                accountAddress: {
-                    oneOf: [
-                        { type: 'string' },
-                        sharedSchemaReferences.PhysicalAddress
-                    ]
-                }
-            }
-        }
-    }
+	additionalProperties: {
+		resolved: {
+			type: "object",
+			properties: {
+				accountAddress: sharedSchemaReferences.PhysicalAddress,
+				accountNumber: { type: "string", pattern: "^[a-zA-Z0-9]{2,80}$" },
+				sortCode: { type: "string" }
+			},
+			required: []
+		},
+		obfuscated: {
+			type: "object",
+			properties: {
+				sortCode: { type: "string" },
+				accountAddress: {
+					oneOf: [
+						{ type: 'string' },
+						sharedSchemaReferences.PhysicalAddress
+					]
+				}
+			}
+		}
+	}
 }
 
 export default fpsSchema;
