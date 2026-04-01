191792a962839a3f2693f6d554fc841fd091d170
ezra ripps <eripps@keeta.com>
2026-04-01 12:50:28 -0400

    add additional pix key type

--- src/services/asset-movement/lib/data/addresses/bank-account/pix.ts
+++ src/services/asset-movement/lib/data/addresses/bank-account/pix.ts
@@ -14,6 +14,7 @@ const pixSchema: AccountAddressSchema = {
 			type: "object",
 			properties: {
 				brCode: { type: "string" },
+				pixKeyType: { type: "string", enum: [ 'random', 'email', 'phone' ] },
 				pixKey: { type: "string" },
 				accountAddress: sharedSchemaReferences.PhysicalAddress,
 				document: {
