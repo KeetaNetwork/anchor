61c8d92240e1213d327fe928c9a6712247c063bc
Tanveer Wahid <twahid@keeta.com>
2026-05-18 09:10:34 -0700

    Minor sonar cloud fix.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,10 @@
-import { AnchorExternal } from './anchor-external.js';
 import * as Certificates from './certificates.js';
 import { EncryptedContainer } from './encrypted-container.js';
 import * as URI from './uri.js';
 
 import Resolver from './resolver.js';
+export { AnchorExternal } from './anchor-external.js';
 export {
-	AnchorExternal,
 	Certificates,
 	EncryptedContainer,
 	Resolver,
