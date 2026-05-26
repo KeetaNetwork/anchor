dcf27ddfd5ad33a1a669075d1be52e1952f9d271
Lars Eidsvoll <leidsvoll@keeta.org>
2026-05-25 21:36:57 -0500

    Add Jamaica (JM) bank-account rail

---
+++ src/services/asset-movement/lib/data/addresses/bank-account/jm.ts
@@ -0,0 +1,40 @@
+import type { AccountAddressSchema } from "../../types.js";
+
+const jamaicaBankAccountSchema: AccountAddressSchema = {
+	type: 'bank-account',
+
+	includeFields: {
+		accountOwner: true,
+		bankName: true,
+		accountNumberEnding: true
+	},
+
+	additionalProperties: {
+		resolved: {
+			type: "object",
+			properties: {
+				bankAccountNumber: {
+					type: "string",
+					description: "Account Number",
+					minLength: 2,
+					maxLength: 80,
+					pattern: "^[a-zA-Z0-9]{2,80}$"
+				},
+				bankCode: {
+					type: "string",
+					description: "Bank Code"
+				},
+				routingCode: {
+					type: "string",
+					description: "Bank Sub Code"
+				}
+			},
+			required: [ 'bankAccountNumber' ]
+		},
+		obfuscated: {
+			type: "object"
+		}
+	}
+}
+
+export default jamaicaBankAccountSchema;
