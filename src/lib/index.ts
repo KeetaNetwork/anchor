2abb364c8333a57969043f09c13deabb0cb52612
Tanveer Wahid <twahid@keeta.com>
2026-05-29 10:02:09 -0700

    Remove unrelated change.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,7 +4,6 @@ import * as URI from './uri.js';
 
 import Resolver from './resolver.js';
 export { AnchorExternal } from './anchor-external.js';
-export { AnchorStatus } from './anchor-status.js';
 export {
 	Certificates,
 	EncryptedContainer,
